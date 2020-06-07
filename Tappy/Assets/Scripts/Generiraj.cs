d0dd8b34019c15a135f25d1b54832d30ae458da0
Matejas S <matejas.santl@student.mev.hr>
2020-06-07 18:30:04 +0200

    gotovo

---
+++ Tappy/Assets/Scripts/Generiraj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generiraj : MonoBehaviour {
+
+	public GameObject stjene;
+	int rezultat=0;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("KreirajStjene", 1f, 1.5f);	
+	}
+	
+	void OnGUI(){
+		GUI.color = Color.black;
+		GUILayout.Label ("Rezultat: " + rezultat.ToString ());
+	}
+	void KreirajStjene(){
+		Instantiate (stjene);
+		rezultat++;
+	}
+}
---
+++ Tappy/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Vector2 jumpForce=new Vector2(0,300);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+			GetComponent<Rigidbody2D> ().AddForce (jumpForce);
+		}
+		Vector2 screenPoints = Camera.main.WorldToScreenPoint (transform.position);
+
+		if (screenPoints.y > Screen.height || screenPoints.y < 0) {
+			Die ();
+		}
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		Die ();
+	}
+	void Die(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
